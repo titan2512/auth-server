729910d592f78055d73ca3677487518d451dbf6c
Roman Sergienko <roman.sergienko@taskdata.com>
2019-03-15 12:53:50 +0400

    Persistence config

--- src/main/java/tech/lmru/auth/AuthServerApplication.java
+++ src/main/java/tech/lmru/auth/AuthServerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-//@EnableJpaRepositories(basePackages = "tech.lmru.repo")
-//@EntityScan("tech.lmru")
+@EnableJpaRepositories(basePackages = "tech.lmru")
 public class AuthServerApplication extends SpringBootServletInitializer {
 
 	@Override
